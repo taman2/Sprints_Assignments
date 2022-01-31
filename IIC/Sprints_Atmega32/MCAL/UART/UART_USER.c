1701072945501d03841f9b1948ad04413e21616b
mohamed salah taman <35311004+taman2@users.noreply.github.com>
2022-01-31 16:49:04 +0200

    Update UART_USER.c

--- IIC/Sprints_Atmega32/MCAL/UART/UART_USER.c
+++ IIC/Sprints_Atmega32/MCAL/UART/UART_USER.c
@@ -11,15 +11,15 @@
 
 
 	struct gstr_USART_Config_t gstr_USART_Config={  .SYNCH_MODE=USART_ASYNCH_MODE               ,
-													.SPEED_MODE=USART_NORMAL_SPEED              ,
-													.TX_=USART_TX_ENABLE                        ,
-													.RX_=USART_RX_ENABLE                        ,
-													.CHAR_SIZE=USART_DATA_FORMAT_8BIT_MODE      ,
-													.PARITY_MODE=USART_PARITY_DISABLE           ,
-													.STOP_BITS_NUM=USART_1_STOP_BIT             ,
-													.TX_INTERRUPT=USART_TX_INTERRUPT_DISABLE    ,
-													.RX_INTERRUPT=USART_RX_INTERRUPT_ENABLE    ,
-													.UDR_INTERRUPT=USART_UDR_INTERRUPT_DISABLE  };
+							.SPEED_MODE=USART_NORMAL_SPEED              ,
+							.TX_=USART_TX_ENABLE                        ,
+							.RX_=USART_RX_ENABLE                        ,
+							.CHAR_SIZE=USART_DATA_FORMAT_8BIT_MODE      ,
+							.PARITY_MODE=USART_PARITY_DISABLE           ,
+							.STOP_BITS_NUM=USART_1_STOP_BIT             ,
+							.TX_INTERRUPT=USART_TX_INTERRUPT_DISABLE    ,
+							.RX_INTERRUPT=USART_RX_INTERRUPT_ENABLE    ,
+							.UDR_INTERRUPT=USART_UDR_INTERRUPT_DISABLE  };
 void USART_RX_ISR(void)
 {
 	
